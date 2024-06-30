0330b1330c33778026f22e86733ad1f3ac8f469f
ledabielic <leda.bielic@gmail.com>
2024-06-30 17:48:35 +0200

    Update add_data.py

--- add_data.py
+++ add_data.py
@@ -1,26 +1,28 @@
-from app import db
-from app.models import Igra, Trener
+from app import create_app, db
+from app.models import Trener, Igra
 
 def dodaj_igre_i_trenere():
     treneri = [
         Trener(ime='Neo', prezime='Čikić'),
         Trener(ime='Josip', prezime='Džinić'),
-        Trener(ime='Lina', prezime='Božić')
+        Trener(ime='Lina', prezime='Bilić')
     ]
-    db.session.bulk_save_objects(treneri)
-    db.session.commit()
 
     igre = [
-        Igra(naziv='Counter-Strike 2', slika='game1.jpg', trener_id=1),
+        Igra(naziv='Counter Strike 2', slika='game1.jpg', trener_id=1),
         Igra(naziv='League of Legends', slika='game2.jpg', trener_id=1),
         Igra(naziv='Apex Legends', slika='game3.jpg', trener_id=2),
         Igra(naziv='Valorant', slika='game4.jpg', trener_id=2),
         Igra(naziv='Tekken', slika='game5.jpg', trener_id=3),
         Igra(naziv='Overwatch 2', slika='game6.jpg', trener_id=3)
     ]
+
+    db.session.bulk_save_objects(treneri)
     db.session.bulk_save_objects(igre)
     db.session.commit()
 
-if __name__ == '__main__':
-    db.create_all()
-    dodaj_igre_i_trenere()
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+        dodaj_igre_i_trenere()
